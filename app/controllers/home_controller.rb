068b42afc2058b31b99575d85d0fabecbff26b95
ckdgns1468 <ckdgns1468@gmail.com>
2016-05-05 02:33:33 +0000

    basic form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def write
+    post = Post.new(title:params[:title], content:params[:content])
+    if post.save
+    redirect_to '/home/index'
+    else
+    render :text=> post.errors.massege[:title].first
+    end
   end
+  
+  def write_reply
+    reply = Reply.new(content:params[:content], post_id:params[:id_of_post])
+    reply.save
+    redirect_to '/home/index'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
-
-  get 'home/write'
+  post '/write' => "home#write"
+  post '/home/reply_write' => "home#reply_write"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
